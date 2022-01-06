dd13cfd9d0bddc15b3f4a7ee33bad8116c6fb8fe
ssilverst <ssilverst@gmail.com>
2022-01-05 22:15:15 -0800

    fixed routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import Home from './components/Home';
 import Digital from './components/Digital'
-import { Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/digital-garden">
-        <Digital />
-      </Route>
+      <Routes>
+        <Route path="/" element={<Home />}>
+        </Route>
+        <Route exact path="/digital" element={<Digital />}>
+        </Route>
+      </Routes>
     </div>
   )
 }
