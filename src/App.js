d81fe3d97eea86566f938bd8b4e6b6da4918ad8a
ssilverst <ssilverst@gmail.com>
2022-01-05 21:28:34 -0800

    adding router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Home from './components/Home';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route exact path="/digital-space">
+          <Home />
+        </Route>
+      </Routes>
     </div>
   )
 }
